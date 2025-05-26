0e2ae6282ef1e9ff368296f6ad967174eb5e2903
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-26 18:22:08 +0700

    memberi hak akses modifier

--- RelasiAndInheritance.cpp
+++ RelasiAndInheritance.cpp
@@ -1,1 +1,16 @@
-RelasiAndInheritance.cpp
+#ifndef ANAK_H
+#define ANAK_H
+
+class anak
+{
+    public:
+    string nama;
+    anak(string pNama) : nama(pNama)
+    {
+        cout <<"Anak\"" << nama << "\" ada \n";
+    }
+    ~anak()
+    {
+        cout << "Anak \"" << nama << "\" tidak ada \n";
+    }
+}
