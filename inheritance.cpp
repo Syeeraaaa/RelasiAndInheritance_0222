07c5c9b28d22bf7f4bf82c0e984c10e22c4ea1a5
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-26 21:46:22 +0700

    membuat desturctor orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 class orang{
-    public:   
+    public:  
+        string nama;
+        
+        orang(string pNama):
+        nama(pNama){
+            cout << "Orang dibuat \n" << endl;
+        }
+        ~orang (){
+            cout << "Orang dihapus \n" << endl;
+        }
 }
