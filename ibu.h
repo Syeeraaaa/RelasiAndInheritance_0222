6d05a4de549d60a8469e24d94206958deb35ed6d
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-26 18:30:51 +0700

    membuat akses modifier untuk class ibu

--- ibu.h
+++ ibu.h
@@ -2,5 +2,12 @@
 #define IBU_H
 
 class Ibu{
-    
+    public:
+    string nama;
+    vector<anak *> daftar_anak;
+
+    ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada \n";
+    }
 }
