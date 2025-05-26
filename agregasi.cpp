7734e13dbee6d7eb00cbae75c15b62f18cb35b48
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-26 18:55:38 +0700

    Menambahkan relasi antar objek ibu dan anak serta mencetak daftar anak

--- agregasi.cpp
+++ agregasi.cpp
@@ -7,5 +7,23 @@ using namespace std;
 
 int main()
 {
-    
+    Ibu *varIbu = new ibu("Dini");
+    Ibu *varIbu2 = new ibu ("novi");
+    anak *varAnak1 = new anak("Tono");
+    anak *varAnak2 = new anak("Rini");
+    anak *varAnak3 = new anak("dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    return 0;
 }
